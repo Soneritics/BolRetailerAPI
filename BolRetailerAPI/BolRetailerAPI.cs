a91f98a07419d828f53fcb3fe9bacb81e6658561
Jordi Jolink <info@soneritics.nl>
2020-02-23 12:42:25 +0100

    Refactor name to comply with standards

--- BolRetailerAPI/BolRetailerAPI.cs
+++ BolRetailerAPI/BolRetailerAPI.cs
@@ -8,20 +8,20 @@ namespace BolRetailerAPI
     /// <summary>
     /// Wrapper class for the Bol Retailer API services.
     /// </summary>
-    public class BolRetailerAPI
+    public class BolRetailerApi
     {
         private readonly AuthorizationToken.AuthorizationToken _authorizationToken;
         private readonly IEndPoints _endPoints;
         private readonly HttpClient _httpClient;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BolRetailerAPI"/> class.
+        /// Initializes a new instance of the <see cref="BolRetailerApi"/> class.
         /// Setup the necessary objects. This object can be used to inject via DI.
         /// </summary>
         /// <param name="clientId">The client identifier.</param>
         /// <param name="clientSecret">The client secret.</param>
         /// <param name="testMode">if set to <c>true</c> [test mode].</param>
-        public BolRetailerAPI(string clientId, string clientSecret, bool testMode = false)
+        public BolRetailerApi(string clientId, string clientSecret, bool testMode = false)
         {
             _endPoints = testMode ? new TestEndPoints() : new EndPoints.EndPoints();
             _httpClient = new HttpClient();
