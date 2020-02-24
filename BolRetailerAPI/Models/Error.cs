03317a443bacac222f50621a5cbdda7f627dd72c
Jordi Jolink <info@soneritics.nl>
2020-02-24 21:51:26 +0100

    Extend error model with properties from API results

--- BolRetailerAPI/Models/Error.cs
+++ BolRetailerAPI/Models/Error.cs
@@ -4,5 +4,11 @@
     {
         public string error { get; set; }
         public string error_description { get; set; }
+        public string type { get; set; }
+        public string title { get; set; }
+        public string status { get; set; }
+        public string detail { get; set; }
+        public string host { get; set; }
+        public string instance { get; set; }
     }
 }
