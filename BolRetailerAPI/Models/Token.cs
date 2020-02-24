08f19f61225d30719cdba9778d85779058ebc189
Jordi Jolink <info@soneritics.nl>
2020-02-24 12:53:07 +0100

    Add token model

--- BolRetailerAPI/Models/Token.cs
+++ BolRetailerAPI/Models/Token.cs
@@ -1,7 +1,25 @@
-﻿namespace BolRetailerAPI.Models
+﻿using System;
+
+namespace BolRetailerAPI.Models
 {
     public class Token
     {
-        
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string scope { get; set; }
+
+        private int _expiresIn;
+        public int expires_in
+        {
+            get => _expiresIn;
+            set
+            {
+                _expiresIn = value;
+                ExpiresAt = Created.AddSeconds(_expiresIn);
+            }
+        }
+
+        public DateTime Created { get; } = DateTime.Now;
+        public DateTime ExpiresAt { get; private set; } = DateTime.Now;
     }
 }
