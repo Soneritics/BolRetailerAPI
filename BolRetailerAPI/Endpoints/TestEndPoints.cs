5b1662c31a0db23f5fe9d6bb17d08a403e3d9508
Jordi Jolink <info@soneritics.nl>
2020-02-24 21:53:32 +0100

    Override only base Uri

--- BolRetailerAPI/Endpoints/TestEndPoints.cs
+++ BolRetailerAPI/Endpoints/TestEndPoints.cs
@@ -6,7 +6,6 @@
     /// <seealso cref="BolRetailerAPI.EndPoints.EndPoints" />
     public class TestEndPoints : EndPoints
     {
-        public new string BaseUriLogin { get; } = "https://login.bol.com/retailer-demo/";
-        public new string BaseUriApiCalls { get; } = "https://api.bol.com/retailer-demo/";
+        public override string BaseUriApiCalls { get; } = "https://api.bol.com/retailer-demo/";
     }
 }
