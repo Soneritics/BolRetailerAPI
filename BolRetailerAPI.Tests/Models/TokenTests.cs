43f3b7e41c889e3c0226da074d439aba1db8f29e
Jordi Jolink <info@soneritics.nl>
2020-02-25 22:38:38 +0100

    Test token expiration

---
+++ BolRetailerAPI.Tests/Models/TokenTests.cs
@@ -0,0 +1,28 @@
+﻿using BolRetailerAPI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BolRetailerAPI.Tests.Models
+{
+    [TestClass]
+    public class TokenTests
+    {
+        /// <summary>
+        /// Test that the token expiration is calculated correctly.
+        /// </summary>
+        [TestMethod]
+        public void Token_Expiration_Is_Correct()
+        {
+            var validityInSeconds = 3;
+
+            var token = new Token()
+            {
+                expires_in = validityInSeconds
+            };
+
+            Assert.AreEqual(
+                token.Created.AddSeconds(validityInSeconds).ToFileTime(), 
+                token.ExpiresAt.ToFileTime()
+            );
+        }
+    }
+}
