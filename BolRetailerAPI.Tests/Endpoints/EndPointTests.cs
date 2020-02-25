3345fea16aa85a884d21d77231130cfea085d07f
Jordi Jolink <info@soneritics.nl>
2020-02-25 22:30:19 +0100

    Test the endpoint classes

---
+++ BolRetailerAPI.Tests/Endpoints/EndPointTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BolRetailerAPI.Tests.Endpoints
+{
+    /// <summary>
+    /// Tests for the EndPoints class
+    /// </summary>
+    [TestClass]
+    public class EndPointTests
+    {
+        /// <summary>
+        /// Ensures the live endpoint usage.
+        /// </summary>
+        [TestMethod]
+        public void Ensure_Test_Endpoint_Usage()
+        {
+            var endpoints = new EndPoints.EndPoints();
+            Assert.AreEqual(
+                "https://api.bol.com/retailer/",
+                endpoints.BaseUriApiCalls
+            );
+        }
+    }
+}
---
+++ BolRetailerAPI.Tests/Endpoints/TestEndPointTests.cs
@@ -0,0 +1,25 @@
+﻿using BolRetailerAPI.EndPoints;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BolRetailerAPI.Tests.Endpoints
+{
+    /// <summary>
+    /// Tests for the TestEndPoints class
+    /// </summary>
+    [TestClass]
+    public class TestEndPointTests
+    {
+        /// <summary>
+        /// Ensures the test endpoint usage.
+        /// </summary>
+        [TestMethod]
+        public void Ensure_Test_Endpoint_Usage()
+        {
+            var endpoints = new TestEndPoints();
+            Assert.AreEqual(
+                "https://api.bol.com/retailer-demo/",
+                endpoints.BaseUriApiCalls
+            );
+        }
+    }
+}
