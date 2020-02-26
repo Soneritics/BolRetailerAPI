ce3d9d417c75cad74b5cb967034da8864f427940
Jordi Jolink <info@soneritics.nl>
2020-02-26 14:58:48 +0100

    Add RateLimits tests

---
+++ BolRetailerAPI.Tests/Models/RateLimitsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BolRetailerAPI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BolRetailerAPI.Tests.Models
+{
+    /// <summary>
+    /// Tests for the RateLimits model.
+    /// </summary>
+    [TestClass]
+    public class RateLimitsTests
+    {
+        /// <summary>
+        /// Test if the rate limits are still valid and should not be refreshed.
+        /// </summary>
+        [TestMethod]
+        public void Is_Valid()
+        {
+            var rateLimits = new RateLimits();
+
+            rateLimits.ResetsAt = DateTime.Now.AddSeconds(10);
+            var isValid = rateLimits.IsStillValid();
+
+            Assert.IsTrue(isValid);
+        }
+
+        /// <summary>
+        /// Test if RateLimits are not valid anymore because they have refreshed.
+        /// </summary>
+        [TestMethod]
+        public void Is_Invalid()
+        {
+            var rateLimits = new RateLimits();
+
+            rateLimits.ResetsAt = DateTime.Now;
+            var isValid = rateLimits.IsStillValid();
+
+            Assert.IsFalse(isValid);
+        }
+    }
+}
